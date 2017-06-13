934c257597b9ece6aaf2a69a6684805970290270
Matthieu Chabert <matthieuchabert@Air-de-Matthieu.homenet.telecomitalia.it>
2017-06-13 10:33:36 +0200

    first branch json into dict

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,11 +1,23 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret1.json', scope)
-client = gspread.authorize(creds)
 
-sheet = client.open("medirechner WS17/18 v02 (Haslreiter)").sheet1
+def main():
 
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+	scope = ['https://spreadsheets.google.com/feeds']
+	print('1')
+	creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+	print('2')
+	client = gspread.authorize(creds)
+	print('3')
+	sheet = client.open("medirechner_WS17_18_v02 (Haslreiter).xlsx").sheet1
+	print('4')
+	list_of_hashes = sheet.get_all_records()
+	print(list_of_hashes)
+
+
+
+
+if __name__ == '__main__':
+	main()
+
